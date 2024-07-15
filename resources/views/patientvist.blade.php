639fd9bfd795026a58d0f6a9a9b6ee462ee078dc
Abdulrehman0202 <mianbrand210@gmail.com>
2024-07-15 11:08:36 +0500

    patients

--- resources/views/patientvist.blade.php
+++ resources/views/patientvist.blade.php
@@ -177,15 +177,78 @@
                 <div>
                     <h1 class="font-bold mx-6 mt-2">Personal information</h1>
                 </div>
-              <form action="#">
-                <label for="">
-                    mvvvvv
-                </label>
-              </form>
+                <div class="maincontentdiv block md:flex">
+                    <div class="formdivcont w-[100%]">
+                        <form action="#">
+                            <div class="grid md:grid-cols-1 gap-6 mx-6 ">
+                                <div>
+                                    <label class="text-[14px] font-bold" for="fullName">@lang('lang.First Name')</label>
+                                    <input type="text" required
+                                        class="w-full border-[#DEE2E6] rounded-[4px] focus:border-primary   h-[40px] text-[14px]"
+                                        name="name" id="dname" placeholder=" @lang('lang.Name_Here')">
+                                </div>
+                                <div>
+                                    <label class="text-[14px] font-bold" for="lastname">@lang('lang.Last Name')</label>
+                                    <input type="text" required
+                                        class="w-full border-[#DEE2E6] rounded-[4px] focus:border-primary   h-[40px] text-[14px]"
+                                        name="l_name" id="l_name" placeholder="@lang('lang.Last Name')">
+                                </div>
+                                <div>
+                                    <label class="text-[14px] font-bold" for="Date of brith">
+                                        @lang('lang.Date of brith')</label>
+                                    <input type="date"
+                                        class="w-full border-[#DEE2E6] mb-11 rounded-[4px] focus:border-primary   h-[40px] text-[14px]"
+                                        name="datebirth" id="datebirth" placeholder="@lang('lang.Date of brith')">
+            
+                                </div>
+                                <button
+                                class="bg-primary text-white rounded-[4px]  mx-6 uaddBtn  font-semibold w-[100%]"
+                                name="save" id="addBtn">
+                                <div class="  hidden" id="spinner">
+                                    <svg aria-hidden="true"
+                                        class="w-[100%] h-5 mx-auto text-center text-gray-200 animate-spin fill-primary"
+                                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                            fill="currentColor" />
+                                        <path
+                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                            fill="currentFill" />
+                                    </svg>
+                                </div>
+                                <div id="text">
+                                    @lang('lang.Update Data')
+                                </div>
+    
+                            </button>
+                            </div>
+                           
+                        </form>
+                    </div>
+                    <div class="w-[100%] overflow-x-auto text-center px-4 ">
+                        <table class="w-full overflow-x-auto border-gray">
+                            <thead>
+                                <tr class="py-6">
+                                    <th>name</th>
+                                    <th>name</th>
+                                    <th>name</th>
+                                    <th>name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="pt-4">
+                                    <td class="text-wrap">laaaaaaaaaa.</td>
+                                    <td class="text-wrap">laaaaaaaaaa.</td>
+                                    <td class="text-wrap">laaaaaaaaaa</td>
+                                    <td class="text-wrap">laaaaaaaaaa </td>
+                        
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-    </div>
-</div>
+            
 
 @include('layouts.footer')
 <script>
